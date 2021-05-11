6c387307d8d13b11192e3cace5a056b2d52c8230
Michał Piątkowski <mcpiatkowski@gmail.com>
2021-05-11 12:13:28 +0200

    add form for test

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { ListGroup, Row, Col } from "react-bootstrap";
+import { ListGroup, Row, Col, Form, Button, ButtonGroup } from "react-bootstrap";
 import axios from "axios";
 
 function HomeScreen() {
   const [wallet, setWallet] = useState([]);
   const [success, setSuccess] = useState(false);
+  const [ticker, setTicker] = useState("");
+  const [quantity, setQuantity] = useState("");
+
   const accessToken = JSON.parse(localStorage.getItem("access"));
 
   useEffect(() => {
@@ -27,25 +30,60 @@ function HomeScreen() {
     }
   }, [success, accessToken]);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("poszlo!");
+    console.log(ticker);
+    console.log(quantity);
+  };
+
   return (
     <div>
       <Row className='my-3'>
-        <h1>Gotówka: 1 000 000 $</h1>
+        <Col>
+          <h3>Łączna wartość:</h3>
+          <p>10 000 000 $</p>
+        </Col>
+        <Col>
+          <h3>Wolna gotówka:</h3>
+          <p> 1 000 000 $</p>
+        </Col>
       </Row>
       <Row>
         <Col>
           <h2> Jakiś wykres </h2>
         </Col>
         <Col>
-          {success && (
-            <ListGroup>
-              {wallet.assets.map((asset, index) => (
-                <ListGroup.Item key={index}>
-                  {asset.ticker}, {asset.name}
-                </ListGroup.Item>
-              ))}
-            </ListGroup>
-          )}
+          <Form onSubmit={handleSubmit}>
+            <Row>
+              <Col>
+                <Form.Control
+                  placeholder='Ticker'
+                  value={ticker}
+                  onChange={(e) => setTicker(e.target.value)}
+                />
+              </Col>
+              <Col>
+                <Form.Control
+                  placeholder='Ilość'
+                  value={quantity}
+                  onChange={(e) => setQuantity(e.target.value)}
+                />
+              </Col>
+            </Row>
+            <Button type='submit'>Dodaj</Button>
+          </Form>
+          <Row>
+            {success && (
+              <ListGroup className='my-3'>
+                {wallet.assets.map((asset, index) => (
+                  <ListGroup.Item key={index}>
+                    {asset.ticker}, {asset.name}
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            )}
+          </Row>
         </Col>
       </Row>
 
