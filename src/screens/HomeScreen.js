e772e914c23e1ea17ac867d5b022c4cb96598451
Michał Piątkowski <mcpiatkowski@gmail.com>
2021-05-11 07:39:07 +0200

    some api calls

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { ListGroup, Row, Col } from "react-bootstrap";
 import axios from "axios";
 
 function HomeScreen() {
@@ -28,8 +29,27 @@ function HomeScreen() {
 
   return (
     <div>
-      <h1>Home Screen</h1>
-      {success && wallet.id}
+      <Row className='my-3'>
+        <h1>Gotówka: 1 000 000 $</h1>
+      </Row>
+      <Row>
+        <Col>
+          <h2> Jakiś wykres </h2>
+        </Col>
+        <Col>
+          {success && (
+            <ListGroup>
+              {wallet.assets.map((asset, index) => (
+                <ListGroup.Item key={index}>
+                  {asset.ticker}, {asset.name}
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+          )}
+        </Col>
+      </Row>
+
+      <Row></Row>
     </div>
   );
 }
